5bdf96b8115ec0740ae8dbc584fb067a3958de88
phat.tran <phattran28052002@gmail.com>
2024-04-16 17:25:07 +0700

    Add controller support to the application

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_api_dnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character { Id = 1, Name = "Phat" }
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.Find(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
